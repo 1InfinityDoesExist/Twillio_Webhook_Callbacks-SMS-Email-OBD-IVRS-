ebcb0dbf3b6156a430152e9eb9ebd9c35f11d454
Avinash Patel <patelavinash508@gmail.com>
2022-12-27 20:21:44 +0530

    Webhook added for obd

--- src/main/java/com/twillio/callback/obd/ObdController.java
+++ src/main/java/com/twillio/callback/obd/ObdController.java
@@ -1,5 +1,10 @@
 package com.twillio.callback.obd;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +12,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
@@ -23,5 +29,28 @@ public class ObdController {
 	public ResponseEntity<?> runObdWebhook(@RequestBody OBDModel obdModel) {
 		obdService.runOBD(obdModel.getToPhoneNumber());
 		return ResponseEntity.status(HttpStatus.OK).body(new ModelMap().addAttribute("msg", "Success"));
+	}
+
+	@RequestMapping(value = "/events", method = RequestMethod.POST)
+	public void obdCallBack(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		log.info("-----Obd service callback-----");
+		String callStatus = request.getParameter("CallStatus");
+		String duration = request.getParameter("Duration");
+		String callDuration = request.getParameter("CallDuration");
+		String sipResponseCode = request.getParameter("SipResponseCode");
+		String recordingSid = request.getParameter("RecordingSid");
+		String recordingUrl = request.getParameter("RecordingUrl");
+		String recordingDuration = request.getParameter("RecordingDuration");
+		String timestamp = request.getParameter("Timestamp");
+		String callbackSource = request.getParameter("CallbackSource");
+		String sequenceNumber = request.getParameter("SequenceNumber");
+
+		// use CallSid
+		log.info("----DialCallerSID : {} and sid :{}", request.getParameter("DialCallSid"),
+				request.getParameter("CallSid"));
+		log.info(
+				" ----CallStatus : {}, duration : {}, callDuration : {}, sipResponseCode : {}, recordingSid : {}, recordingUrl :{}, recordingDuration :{}, timestamp : {}, callBackSource : {}, sequenceNumber : {}",
+				callStatus, duration, callDuration, sipResponseCode, recordingSid, recordingUrl, recordingDuration,
+				timestamp, callbackSource, sequenceNumber);
 	}
 }
